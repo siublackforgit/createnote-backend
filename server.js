9b4e0c18324ddaeaa38645d8fa49d48bf58d65c8
Ricky Yeung <70504085+siublackforgit@users.noreply.github.com>
2023-01-29 21:00:33 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const path = require("path");
+
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(express.json())
+app.use("/",require("./routes/noteRoute"));
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("Databse connected")
+    app.listen(8000,function(){
+        console.log("Server is running on port 8000")
+    })
+    
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
